530bc9141240c1288cb3ba6953d40e0dfbb9ddaa
mdjemaa <mdjemaa@student.42mulhouse.fr>
2024-01-18 19:23:57 +0100

    pouet

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -3,17 +3,22 @@
 #include <stack>
 #include <algorithm>
 
-template <typename T>
+template <typename T, Container=deque>
 class MutantStack: public std::stack<T> {
 public :
+    typedef typename std::deque<T>::iterator iterator;
+
     MutantStack() {};
     MutantStack(MutantStack const &ref) { *this = ref; }
     ~MutantStack() {};
 
     MutantStack &operator=(MutantStack const &ref) {
-        std::stack<int>::operator= ref;
+        this->c = ref.c;
         return *this;
     }
+
+    std::deque<T>::iterator begin() { return this->c.begin(); }
+    std::deque<T>::iterator end() { return this->c.end(); }
 
 private:
 
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -10,23 +10,23 @@ int main()
     std::cout << mstack.size() << std::endl;
     mstack.push(3);
     mstack.push(5);
+    mstack.push(0);
     mstack.push(737);
     //[...]
-    mstack.push(0);
     MutantStack<int> mstack2(mstack);
     std::cout << "ms : top : "<< mstack.top() << std::endl;
     std::cout << mstack.size() << std::endl;
     std::cout << "ms2 : top : "<< mstack2.top() << std::endl;
     std::cout << mstack2.size() << std::endl;
-    // MutantStack<int>::iterator it = mstack.begin();
-    // MutantStack<int>::iterator ite = mstack.end();
-    // ++it;
-    // --it;
-    // while (it != ite)
-    // {
-    //     std::cout << *it << std::endl;
-    //     ++it;
-    // }
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+    ++it;
+    --it;
+    while (it != ite)
+    {
+        std::cout << *it << std::endl;
+        ++it;
+    }
     std::stack<int> s(mstack);
     return 0;
 }
